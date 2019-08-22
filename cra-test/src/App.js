b71d6a31ed725ba89bc292add0824b56b3e9031f
diesluna <kyang2@gmail.com>
2019-08-22 18:58:35 +0900

    #1 chapter1 todo lists

--- cra-test/src/App.js
+++ cra-test/src/App.js
@@ -1,27 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import TodoList from "./TodoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>TEST CODE GOGOGOGOGO!!!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <p>TEST CODE GOGOGOGOGO!!!</p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TodoList />
+      </div>
+    );
+  }
 }
+
+console.log(`NODE_ENV = ${process.env.NODE_ENV}`);
 
 export default App;
